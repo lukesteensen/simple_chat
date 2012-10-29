e65621181523997a8c979250dd6cb2bdb47068a5
Luke Steensen <luke.steensen@gmail.com>
2012-10-29 16:43:22 -0500

    rearrage some js function definitions

--- public/js/application.js
+++ public/js/application.js
@@ -1,6 +1,7 @@
 $(function () {
 
-  client = new Faye.Client('http://localhost:8080/faye');
+  var client = new Faye.Client('http://localhost:8080/faye');
+  var messageDiv = $(".messages");
 
   client.subscribe('/*', function(msg) {
     var html = '<p data-user="' + user_id +
@@ -11,32 +12,11 @@ $(function () {
     messageDiv.children("p").last()[0].scrollIntoView();
   });
 
-  // UUIDs courtesy of http://stackoverflow.com/a/2117523/394282
-  var user_id = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
-    return v.toString(16);
-  });
-
-  // function to generate/memoize unique colors per user id
-  var user_color = (function() {
-    var user_colors = {};
-    // stolen from: http://paulirish.com/2009/random-hex-color-code-snippets/
-    var randColor = function() { return '#'+Math.floor(Math.random()*16777215).toString(16); };
-    user_colors[user_id] = randColor();
-    var get_color = function(user) {
-      var existing = user_colors[user];
-      if ( existing === undefined ) {
-        user_colors[user] = randColor();
-        return user_colors[user];
-      } else {
-        return existing;
-      }
-    };
-    return get_color;
-  })();
-
-
-  var messageDiv = $(".messages");
+  var send = function() {
+    var msg = $("input[type=text]").val();
+    client.publish("/chan", {text: msg, timestamp: new Date().getTime(), sender_id: user_id});
+    $("input[type=text]").val("");
+  };
 
   $("input[type=button]").click(function() {
     send();
@@ -51,10 +31,27 @@ $(function () {
 
 });
 
-function send() {
-  var msg = $("input[type=text]").val();
-  client.publish("/chan", {text: msg, timestamp: new Date().getTime(), sender_id: user_id});
-  $("input[type=text]").val("");
-}
+// UUIDs courtesy of http://stackoverflow.com/a/2117523/394282
+var user_id = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+  var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+  return v.toString(16);
+});
 
+// function to generate/memoize unique colors per user id
+var user_color = (function() {
+  var user_colors = {};
+  // stolen from: http://paulirish.com/2009/random-hex-color-code-snippets/
+  var randColor = function() { return '#'+Math.floor(Math.random()*16777215).toString(16); };
+  user_colors[user_id] = randColor();
+  var get_color = function(user) {
+    var existing = user_colors[user];
+    if ( existing === undefined ) {
+      user_colors[user] = randColor();
+      return user_colors[user];
+    } else {
+      return existing;
+    }
+  };
+  return get_color;
+})();
 
